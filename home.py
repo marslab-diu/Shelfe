5290cb88b879e731ca64dc04bf03b058184c95fd
Sourav Garodia <96367006+garodiaa@users.noreply.github.com>
2024-12-10 13:28:27 +0600

    hahah

--- home.py
+++ home.py
@@ -1,9 +1,21 @@
 import streamlit as st
 
 def app():
-    hero_image = "UI\1.png"
+    hero = "UI\I1.png"
+    all_books = "UI\I2.png"
+    request = "UI\I3.png"
+    features = "UI\I4.png"
+    faq = "UI\I5.png"
+    footer = "UI\I6.png"
 
-    st.image(hero_image, use_container_width=True)
+
+    st.image(hero, use_container_width=True)
+    st.image(all_books, use_container_width=True)
+    st.image(request, use_container_width=True)
+    st.image(features, use_container_width=True)
+    st.image(faq, use_container_width=True)
+    st.image(footer, use_container_width=True)
+
 
 
     
